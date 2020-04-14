4967dbfd45441c5b91181e8b8249e069b11e53fa
Lincoln Youree <lincolnyouree@Lincolns-Air.attlocal.net>
2020-04-14 10:52:07 -0500

    Updates

---
+++ precache-manifest.ffe5a1c5ad74909360b8ef894e71986c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6281f8d7d0729a85fc246973ab3ed8",
+    "url": "/USA-Circle-Selector/index.html"
+  },
+  {
+    "revision": "2e8e31b6f59b85648be0",
+    "url": "/USA-Circle-Selector/static/css/main.36b5c7d0.chunk.css"
+  },
+  {
+    "revision": "d63d50c3bd285730e01b",
+    "url": "/USA-Circle-Selector/static/js/2.7d24785f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/USA-Circle-Selector/static/js/2.7d24785f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8e31b6f59b85648be0",
+    "url": "/USA-Circle-Selector/static/js/main.6cbf9cb8.chunk.js"
+  },
+  {
+    "revision": "cb9d4d0f9617318f062e",
+    "url": "/USA-Circle-Selector/static/js/runtime-main.8ef1dc94.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3aa856e9ce721da0bf2d5bfa8f64fd08.js"
+  "/USA-Circle-Selector/precache-manifest.ffe5a1c5ad74909360b8ef894e71986c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/USA-Circle-Selector/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

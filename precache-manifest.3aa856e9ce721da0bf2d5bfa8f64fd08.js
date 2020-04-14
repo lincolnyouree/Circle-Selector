b0dbc52e241b8dfa73a92b13a74a9194f7fe6c1c
Lincoln Youree <lincolnyouree@Lincolns-Air.attlocal.net>
2020-04-14 10:47:21 -0500

    Updates

---
+++ precache-manifest.3aa856e9ce721da0bf2d5bfa8f64fd08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dcace6a883a5635a359cdbac4a0ef6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "034df0d9e9a2ea2ef124",
+    "url": "/static/css/main.36b5c7d0.chunk.css"
+  },
+  {
+    "revision": "d63d50c3bd285730e01b",
+    "url": "/static/js/2.7d24785f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d24785f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034df0d9e9a2ea2ef124",
+    "url": "/static/js/main.2334a020.chunk.js"
+  },
+  {
+    "revision": "cccd63c4c08ac9b7790b",
+    "url": "/static/js/runtime-main.7479490d.js"
+  }
+]);
